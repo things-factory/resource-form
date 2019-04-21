1a025fd69dc06e2a2d99e6e2d92de1d1e26dc905
Hearty Oh <heartyoh@hatiolab.com>
2019-04-22 02:14:52 +0800

    v0.1.7

--- src/components/simple-grid/simple-grid-footer.js
+++ src/components/simple-grid/simple-grid-footer.js
@@ -1,17 +1,23 @@
 import { LitElement, html, css } from 'lit-element'
 
+import '@material/mwc-icon/mwc-icon'
+
 class SimpleGridFooter extends LitElement {
   constructor() {
     super()
 
     this.data = []
     this.total = 0
+    this.page = 0
+    this.limit = 50
   }
 
   static get properties() {
     return {
       data: Array,
-      total: Number
+      total: Number,
+      page: Number,
+      limit: Number
     }
   }
 
@@ -20,7 +26,7 @@ class SimpleGridFooter extends LitElement {
       css`
         :host {
           display: flex;
-          flex-direction: column;
+          flex-direction: row;
 
           overflow: hidden;
 
@@ -28,23 +34,67 @@ class SimpleGridFooter extends LitElement {
           line-height: var(--grid-footer-height, 24px);
         }
 
-        span {
+        :host * {
           white-space: nowrap;
           overflow: hidden;
           text-overflow: ellipsis;
 
-          text-align: right;
+          text-align: center;
 
           background-color: var(--grid-footer-background-color, gray);
           color: var(--grid-footer-color, white);
+        }
+
+        span {
+          height: var(--grid-footer-height, 24px);
+        }
+
+        mwc-icon {
+          font-size: 1.5em;
+          vertical-align: middle;
+        }
+
+        span.limit a {
+          color: lightgray;
+        }
+
+        span.limit a[selected] {
+          color: white;
+          font-weight: bold;
+        }
+
+        .filler {
+          flex: 1;
         }
       `
     ]
   }
 
+  get totalPage() {}
+
   render() {
+    var data = this.data || []
+    var begin = data.length * this.page + 1
+    var end = begin + data.length - 1
+
     return html`
-      <span>total ${(this.data || []).length}/${this.total || 0} records.</span>
+      <a><mwc-icon>skip_previous</mwc-icon></a>
+      <a><mwc-icon>navigate_before</mwc-icon></a>
+      <span>page ${this.page + 1}&nbsp;/&nbsp;${Math.ceil(this.total / this.limit)}</span>
+      <a><mwc-icon>navigate_next</mwc-icon></a>
+      <a><mwc-icon>skip_next</mwc-icon></a>
+      <span class="filler"></span>
+      <span class="limit">
+        <a ?selected=${this.limit == 20}>20</a>
+        <a ?selected=${this.limit == 30}>30</a>
+        <a ?selected=${this.limit == 50}>50</a>
+        <a ?selected=${this.limit == 100}>100</a>
+        items
+      </span>
+      <span>&nbsp;</span>
+      <span>${begin} - ${end}</span>
+      <span>&nbsp;/&nbsp;</span>
+      <span>total ${this.total || 0} records.</span>
     `
   }
 }
