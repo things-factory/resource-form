7f74872b72caee5eb73d6bf58a5013d673890179
leemyunggyu <jaylee@hatiolab.com>
2019-05-02 15:26:21 +0800

    update context action bar

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,17 @@
 import { store } from '@things-factory/shell'
 import { addRoutingType } from '@things-factory/menu-base'
 
+import { APPEND_FOOTERBAR, TOOL_POSITION } from '@things-factory/layout-base'
+
 export default function bootstrap() {
+  import('./components/page-action-context-bar')
   store.dispatch(addRoutingType('RESOURCE', 'resource'))
+
+  store.dispatch({
+    type: APPEND_FOOTERBAR,
+    template: html`
+      <page-action-context-bar></page-action-context-bar>
+    `,
+    position: TOOL_POSITION.REAR_END
+  })
 }
---
+++ src/components/page-action-context-bar.js
@@ -0,0 +1,50 @@
+import { connect } from 'pwa-helpers/connect-mixin'
+import { LitElement, html, css } from 'lit-element'
+
+import { store } from '@things-factory/shell'
+
+class PageActionContextBar extends connect(store)(LitElement) {
+  static get properties() {
+    return {
+      actions: Array
+    }
+  }
+
+  static get styles() {
+    return [
+      css`
+        :host {
+          margin: auto 0px auto auto;
+        }
+      `
+    ]
+  }
+
+  render() {
+    return html`
+      ${this.actions.map(
+        action => html`
+          ${action.select && action.select.length > 0
+            ? html`
+                <select @change="${action.action}">
+                  ${action.select.map(
+                    option => html`
+                      <option>${option}</option>
+                    `
+                  )}
+                </select>
+              `
+            : html`
+                <button @click="${action.action}">${action.title}</button>
+              `}
+        `
+      )}
+    `
+  }
+
+  stateChanged(state) {
+    this._actions = (state.route && state.route.context && state.route.context.actions) || []
+  }
+}
+
+customElements.define('page-action-context-bar', PageActionContextBar)
