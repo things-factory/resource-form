5f95623191d27d855741eb44665a814d6ffb678e
Hearty Oh <heartyoh@hatiolab.com>
2019-07-17 22:03:49 +0900

    v0.2.0

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -3,7 +3,8 @@ import { html } from 'lit-html'
 import { store } from '@things-factory/shell'
 import { addRoutingType } from '@things-factory/menu-base'
 
-import { APPEND_CONTEXT_TOOL, TOOL_POSITION } from '@things-factory/layout-base'
+import { TOOL_POSITION } from '@things-factory/layout-base'
+import { APPEND_CONTEXT_TOOL } from '@things-factory/context-base'
 
 export default function bootstrap() {
   import('./components/page-action-context-bar')
