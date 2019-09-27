f948eb28f6c09f8ca17d198e4138e9e71f828841
Hearty Oh <heartyoh@hatiolab.com>
2019-09-27 13:28:28 +0800

    Update code-editor.js

--- client/data-grist/editors/code-editor.js
+++ client/data-grist/editors/code-editor.js
@@ -5,18 +5,6 @@ import '@material/mwc-icon'
 import { client } from '@things-factory/shell'
 import { InputEditor } from '@things-factory/grist-ui'
 
-/* 
-  TODO
-  - 코드리스트 fetch 후에 어떻게 템플릿에 반영할까 ? 
-    - 셀렉트가 최초에 열리는 시점에 fetch한다면, update할 수 있는 방법이 필요하다.
-    - 셀렉트가 열리기 전에 fetch한다면, (사용자가) 에디터를 다시 열어서 반영할 수도 있겠다. (언제 어떻게 fetch를 트리거링 할 수 있느냐가 관건.)
-  - 최초에 fetch해 온 코드를 어디에 저장해두고 공유할까 ?
-  - 코드 공유는 어느 단위 안에서 할까 ? 그리드 컬럼 단위. 따라서, 컬럼에 데이터 저장 및 사용을 위한 방법 제공이 필요하다.
-    - this.column.options._codes = ...
-  - 어느 시점에 코드를 refetch할까 ? 그리드가 새로 만들어질 때 ? 그리드가 새로 configuration 될 때 ?
-  - 다국어는 어떻게 처리할까 ? 코드 디스크립션이 언어에 따라서 정의되지 않으므로 다국어는 생각하지 않는다.
-*/
-
 const FETCH_COMMON_CODE_GQL = codeName => gql`
 {
   commonCode(name: "${codeName}") {
@@ -30,31 +18,41 @@ const FETCH_COMMON_CODE_GQL = codeName => gql`
 `
 
 export class CodeEditor extends InputEditor {
-  async fetchCode(codeName) {
-    var response = await client.query({
-      query: FETCH_COMMON_CODE_GQL(codeName)
-    })
+  async firstUpdated() {
+    super.firstUpdated()
 
-    if (response) {
-      this.column.record.codes = response.data.commonCode.details.sort(function(a, b) {
-        return a.rank - b.rank
+    var { codeName, codes } = this.column.record || {}
+
+    if (!codes && codeName) {
+      /* codeName으로 fetch 해와서, this.column.record.codes에 보관한다. */
+      var response = await client.query({
+        query: FETCH_COMMON_CODE_GQL(codeName)
       })
+
+      var commonCode = response && response.data && response.data.commonCode
+
+      if (commonCode) {
+        this.column.record.codes = [{ name: '', description: '' }].concat(
+          commonCode.details.sort(function(a, b) {
+            return a.rank - b.rank
+          })
+        )
+
+        this.requestUpdate()
+      }
     }
   }
 
   get editorTemplate() {
-    var { codeName, codes } = this.column.record || {}
-
-    if (!codes && codeName) {
-      /* 1. codeName으로 fetch 해와서, this.column.record.codes에 보관한다. */
-      this.fetchCode(codeName)
-    }
+    var { codes } = this.column.record || {}
 
     return html`
       <select>
         ${(codes || ['']).map(
           code => html`
-            <option ?selected=${code.name == this.value}>${code.name} - ${code.description}</option>
+            <option ?selected=${code.name == this.value} value=${code.name}
+              >${code.name}${code.description ? ` - ${code.description}` : ''}</option
+            >
           `
         )}
       </select>
